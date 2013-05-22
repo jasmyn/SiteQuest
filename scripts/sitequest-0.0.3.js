598c2d816b594aa59e966d8ed23269acd78c1c0a
Jasmyn Bianca <jasmynbianca@gmail.com>
2013-05-22 11:18:16 -0600

    added path2Images config property

--- scripts/sitequest-0.0.3.js
+++ scripts/sitequest-0.0.3.js
@@ -9,7 +9,7 @@
 /* Edit the sqConfig variable */
 var sqConfig = {
 	'xpPerLink': 100, // how much XP to award per link followed
-	'path2Images': '/img/SiteQuest/'
+	'path2Images': './img/SiteQuest/'
 };
 
 var siteQuest = {
@@ -39,6 +39,8 @@ var siteQuest = {
 };
 
 (function ($) {
+	var xpPerLink = sqConfig.xpPerLink, score = siteQuest.getScore(), path2Images = sqConfig.path2Images;
+
 	//slideout
 	$('#sq-button').click(function() {
 		$('#site-quest').slideToggle('slow', function() {
@@ -47,7 +49,6 @@ var siteQuest = {
 	});
 
 	//set score
-	xpPerLink = sqConfig.xpPerLink;
 	$('a').click(function () {
 		siteQuest.setScore(xpPerLink);
 	});
@@ -57,40 +58,38 @@ var siteQuest = {
 	$('#sq-reset').click(function () {
 		siteQuest.resetScore();
 		$('#site-quest').children().first().text('SiteQuest:' + siteQuest.getScore());
-		$('#sq-img').attr('src', 'images/SiteQuest/level-0.png');
-	})
+		$('#sq-img').attr('src', path2Images + 'level-0.png');
+	});
 
 	//set image
-	var score = siteQuest.getScore(), path2Images = sqConfig.path2Images;
-
 	if (score >= 100 && score < 200) { 
 		$('#sq-img').attr('src', path2Images + 'level-1.png');
-	};
+	}
 	if (score >= 200 && score < 300) { 
 		$('#sq-img').attr('src', path2Images + 'level-2.png');
-	};
+	}
 	if (score >= 300 && score < 400) { 
 		$('#sq-img').attr('src', path2Images + 'level-3.png');
-	};
+	}
 	if (score >= 400 && score < 500) { 
 		$('#sq-img').attr('src', path2Images + 'level-4.png');
-	};
+	}
 	if (score >= 500 && score < 600) { 
 		$('#sq-img').attr('src', path2Images + 'level-5.png');
-	};
+	}
 	if (score >= 600 && score < 700) { 
 		$('#sq-img').attr('src', path2Images + 'level-6.png');
-	};
+	}
 	if (score >= 700 && score < 800) { 
 		$('#sq-img').attr('src', path2Images + 'level-7.png');
-	};
+	}
 	if (score >= 800 && score < 900) { 
 		$('#sq-img').attr('src', path2Images + 'level-8.png');
-	};
+	}
 	if (score >= 900 && score < 1000) { 
 		$('#sq-img').attr('src', path2Images + 'level-9.png');
-	};
+	}
 	if (score >= 1000) { 
-		$('#sq-img').attr('src', 'level-10.png');
-	};
+		$('#sq-img').attr('src', path2Images + 'level-10.png');
+	}
 }(jQuery));
